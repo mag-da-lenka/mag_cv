330bce98d7c5bcae2544b58fa85c2f518640c7ac
mag-da-lenka <magdalenka.ag@gmail.com>
2021-02-11 20:38:46 +0100

    sketch3

--- design_code_interact/cv_main.js
+++ design_code_interact/cv_main.js
@@ -29,15 +29,20 @@ const acc = document.getElementsByClassName("accordion");
 // var i;
 
 for (i = 0; i < acc.length; i++) {
+
     acc[i].addEventListener("click", function () {
+        
         this.classList.toggle("active");
+
         const panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
         } else {
             panel.style.maxHeight = panel.scrollHeight + "px";
         }
+
     });
+
 }
 
 
