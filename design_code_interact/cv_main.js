18c35a92b6519404c1f9c7079fb4e09f765d027c
mag-da-lenka <magdalenka.ag@gmail.com>
2021-11-03 20:40:07 +0100

    aurora is here

--- design_code_interact/cv_main.js
+++ design_code_interact/cv_main.js
@@ -95,7 +95,7 @@ showAutoSlides();
 
 const openMobileNav =
     () => { document.getElementById("mobileNav").style.height = "100%"; }
-    
+
 const closeMobileNav =
     () => { document.getElementById("mobileNav").style.height = "0%"; }
 
@@ -110,10 +110,27 @@ const closeMobileNav =
 
 
 
+const holographicElement = document.getElementById("holographic");
 
+function updateHolographicBackground(value) {
+    const percentage = value * 100;
+    holographicElement.style.backgroundPosition = percentage + "%";
+}
 
+function handleMouseMove(event) {
+    const x = event.clientX;
+    const width = document.documentElement.clientWidth;
+    const value = x / width;
+    updateHolographicBackground(value);
+}
 
+function handleDeviceOrientation(event) {
+    const z = Math.abs(event.alpha); // rotation degrees from 0 to 360
+    const value = z / 360;
+    updateHolographicBackground(value);
+}
 
+window.addEventListener("deviceorientation", handleDeviceOrientation, true);
 
 
 
