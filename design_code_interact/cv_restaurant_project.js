c3aa567afc83a58cc5963b65b828bd2b1df6ac42
mag-da-lenka <magdalenka.ag@gmail.com>
2022-05-05 13:02:59 +0200

    projects extracted

--- design_code_interact/cv_restaurant_project.js
+++ design_code_interact/cv_restaurant_project.js
@@ -4,32 +4,23 @@ console.table(`hello Hello green love`)
 
 
 
-// HOLOGRAPHIC BODY 
+// accordion
 
-const holoBody = document.getElementById("holo_fun");
+const acc = document.getElementsByClassName("accordion");
 
-// get mouse X position in mili
-const handleMouseMove = (event) => {
+for (i = 0; i < acc.length; i++) {
 
-    console.log(`1) new_move: `)
+    acc[i].addEventListener("click", function () {
 
-    const positionX_value = event.clientX;
-    console.log(`2) positionX_value: `, positionX_value); //  2) positionX_value:  522
+        this.classList.toggle("active");
 
-    const clientWidth_value = document.documentElement.clientWidth;
-    console.log(`3) clientWidth_value: `, clientWidth_value) //  3) clientWidth_value:  1536
+        const panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "vw";
+        }
 
-    const calculated_value_mili = positionX_value / clientWidth_value;
-    console.log(`4) calculated_value_mili: `, calculated_value_mili.toFixed(3)) //  4) calculated_value_mili:  0.340
+    });
 
-    updateHolographicBackground(calculated_value_mili);
-}
-
-// mouse X position in %
-const updateHolographicBackground = (O_xxx) => {
-
-    const percentage_value = O_xxx * 100; // 5) percentage_value:  0.000 (initial)
-    console.log(`5) percentage_value: `, percentage_value.toFixed(3)) // 5) percentage_value:  33.984
-
-    holoBody.style.backgroundPosition = percentage_value + "%";
 }
